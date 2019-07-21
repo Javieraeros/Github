96eaf87656d11d59462c751632734a2e6953c52a
Javier Ruiz <franciscoj.ruiz@s-dos.es>
2019-07-22 01:49:23 +0200

    Added authorization header with Basic token

--- data/src/main/java/es/fjruiz/data/net/RestApiImpl.kt
+++ data/src/main/java/es/fjruiz/data/net/RestApiImpl.kt
@@ -4,12 +4,13 @@ import com.google.gson.GsonBuilder
 import es.fjruiz.data.BuildConfig
 import es.fjruiz.data.entity.UserEntity
 import okhttp3.Dispatcher
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RestApiImpl: RestApi {
+class RestApiImpl : RestApi {
     //region Static
     companion object {
         val TAG = RestApiImpl::class.java.simpleName
@@ -44,9 +45,11 @@ class RestApiImpl: RestApi {
         val retrofit = Retrofit.Builder()
             .client(
                 OkHttpClient.Builder()
-                .addInterceptor(httpInterceptor)
-                .dispatcher(dispatcher)
-                .build())
+                    .addInterceptor(httpInterceptor)
+                    .addInterceptor(getHeaderInterceptor())
+                    .dispatcher(dispatcher)
+                    .build()
+            )
             .baseUrl(baseUrl)
             .addConverterFactory(gsonConverter)
             .build()
@@ -67,7 +70,13 @@ class RestApiImpl: RestApi {
     //endregion
 
     //region Private methods
+    private fun getHeaderInterceptor(): Interceptor = Interceptor {
+        val builder = it.request().newBuilder()
+        builder.addHeader("Accept", "application/json")
+        builder.addHeader("Authorization", "token ${BuildConfig.GITHUB_TOKEN}")
 
+        it.proceed(builder.build())
+    }
     //endregion
 
 }
